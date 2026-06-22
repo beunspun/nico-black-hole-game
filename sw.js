3e872febe78c6762c8e8ee235278c69658eaacc4
Joseph Wilde <josephwilde@Josephs-MacBook-Pro.local>
2026-06-22 13:02:16 +0100

    Update game (black-hole-20260622130215)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker for Lab Escape - Black Hole Game (offline-capable PWA) */
-const CACHE = 'black-hole-20260611203914';
+const CACHE = 'black-hole-20260622130215';
 const ASSETS = [
   './black-hole-game.html',
   './manifest.webmanifest',
